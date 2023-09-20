6c6192ca5c41355969c3c7847ba5456c68145c0b
Jospeeth <japertuzd@gmail.com>
2023-09-20 15:54:05 -0500

    Mark task as done

--- toDo.js
+++ toDo.js
@@ -15,11 +15,27 @@ function saveTask() {
     console.log(tasks);
 
     let elementList = document.createElement("li");
+    let check = document.createElement("input");
+    check.type="checkbox";
+    check.className= "task-checkbox "
     elementList.textContent = task;
     
+        // Agregar un controlador de eventos al checkbox
+        check.addEventListener("change", function() {
+          if (check.checked) {
+            elementList.classList.add("checked"); // Aplicar la clase CSS cuando se marque el checkbox
+          } else {
+            elementList.classList.remove("checked"); // Quitar la clase CSS cuando se desmarque el checkbox
+          }
+        });
+    
+
     document.getElementById("input-task").value = "";
     let lista = document.getElementById("list-todo");
-    lista.appendChild(elementList);
+
+    lista.appendChild(elementList).appendChild(check);
     closeModal();
   }
 }
+
+
